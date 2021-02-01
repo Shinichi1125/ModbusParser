1d640185801ad7fed77b36e2fcff2a94e9192f46
stakagi <Shinichi.Takagi@ha.ax>
2021-02-01 05:56:23 +0200

    update docker files

---
+++ docker/tuf.sql
@@ -0,0 +1,24 @@
+
+create database tuf
+
+use tuf;
+
+CREATE TABLE register (
+	id int NOT NULL AUTO_INCREMENT,
+    reg21 int,
+    reg22 int,
+    reg92 int,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO register VALUES (NULL, 65480, 65535, 806);
+INSERT INTO register VALUES (NULL, 65470, 65535, 796);
+INSERT INTO register VALUES (NULL, 65460, 65535, 786);
+INSERT INTO register VALUES (NULL, 65475, 65535, 801);
+INSERT INTO register VALUES (NULL, 65490, 65535, 816);
+
+SELECT * FROM register;
+
+#DELETE FROM register WHERE id = 6;
+
+DROP TABLE register;
--- modbus-parser-ui/src/Constants.ts
+++ modbus-parser-ui/src/Constants.ts
@@ -1,2 +1,2 @@
 //export const API_URL = 'http://localhost:8080/modbusparserservices/api';
-export const API_URL = 'http://modbusparser/modbusparserservices/api';
+export const API_URL = 'http://modbusparser.work/modbusparserservices/api';
